9bbd4f7cb8f5250362df3a7f5ab1b38d0d834249
mamzi <mr.pourbaba@gmail.com>
2024-01-26 17:39:01 +0330

    initial

---
+++ function/FunctionHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public string Handle(string input) {
+            string output="";
+            string[] lines = input.Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+            {
+                
+                if (lines[i].Length > 10)
+                {
+                    lines[i] = lines[i][0] + (lines[i].Length - 2).ToString() + lines[i][lines[i].Length - 1];
+                }
+            }
+            for (int i = 0; i < lines.Length; i++)
+            {
+                output+=lines[i]+"\n";
+            }
+            return output;
+        }
+    }
+}
